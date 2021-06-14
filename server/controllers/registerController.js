ac0c885ad9eb5559ff74539420ab7e5ed1b199ed
davidspanjaard <dmspanjaard@gmail.com>
2021-06-14 13:22:08 +0200

    feat: Connect Register controller to postgres db

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,23 +1,22 @@
-// const User = require('../models/userModel.js');
+const db = require('../models/postgres.js');
 
 const registerHandler = async (req, res) => {
   try {
-    //Checks if email is available
-    // const user = await User.findOne({
-    //   where: {
-    //     email: req.body.email,
-    //   },
-    // });
-    const user = null;
-    if (user) {
+    const foundUser = await db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (foundUser) {
       res.status(400).send('User Already exists');
     } else {
-      // const user = await User.create({
-      //   email: req.body.email,
-      //   username: req.body.username,
-      //   password: req.body.password,
-      // });
+      const user = await db.User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
       res.status(201).send(user);
+      // }
     }
   } catch (error) {
     res.status(400);
