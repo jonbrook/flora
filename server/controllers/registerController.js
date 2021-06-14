6f2067b6f68f9468a78a6f4bee4f891d3d99f476
davidspanjaard <dmspanjaard@gmail.com>
2021-06-14 11:30:14 +0200

    refactor: tested logic of controller

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,21 +1,22 @@
-const User = require('../models/userModel.js');
+// const User = require('../models/userModel.js');
 
 const registerHandler = async (req, res) => {
   try {
     //Checks if email is available
-    const user = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+    // const user = await User.findOne({
+    //   where: {
+    //     email: req.body.email,
+    //   },
+    // });
+    const user = null;
     if (user) {
       res.status(400).send('User Already exists');
     } else {
-      const user = await User.create({
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-      });
+      // const user = await User.create({
+      //   email: req.body.email,
+      //   username: req.body.username,
+      //   password: req.body.password,
+      // });
       res.status(201).send(user);
     }
   } catch (error) {
