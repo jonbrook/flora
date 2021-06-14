c85d1a3db6edae86f8ca49f49a1eb4fecc418604
davidspanjaard <dmspanjaard@gmail.com>
2021-06-14 15:51:43 +0200

    fix: typos in controller definition

---
+++ server/controllers/plantsByUserController.js
@@ -0,0 +1,39 @@
+const db = require('../models/postgres');
+
+const getPlantsByUserHandler = async (req, res) => {
+  try {
+    const { userEmail } = req.params;
+    const plantsByUser = await db.PlantsByUser.findAll({
+      where: { email: userEmail },
+    });
+    if (plantsByUser) {
+      res.status(200).send(plantsByUser);
+    } else {
+      res.status(400).send('could not get users');
+    }
+  } catch (error) {
+    res.status(400);
+  }
+};
+
+const postPlantsByUserHandler = async (req, res) => {
+  try {
+    console.log('db', db.PlantsByUser);
+    const user = await db.PlantsByUser.create({
+      email: req.body.email,
+      scientificName: req.body.scientificName,
+      pictureURL: req.body.pictureURL,
+      lastWatered: new Date(),
+    });
+    console.log(user);
+    res.status(201).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+  }
+};
+
+module.exports = {
+  getPlantsByUserHandler,
+  postPlantsByUserHandler,
+};
