52477e320a3b5bc4da1c631eb3694d8707d5db23
davidspanjaard <dmspanjaard@gmail.com>
2021-06-14 15:54:05 +0200

    feat: api endpoints finalised

--- server/router.js
+++ server/router.js
@@ -5,17 +5,18 @@ const loginController = require('./controllers/loginController');
 const registerController = require('./controllers/registerController');
 const plantsController = require('./controllers/plantController');
 const plantsByUserController = require('./controllers/plantsByUserController');
-const userController = require('./controllers/user');
+const userController = require('./controllers/userController');
 
 router.post('/login', loginController.loginHandler);
 
-router.post('/register', registerController.registerController);
+router.post('/register', registerController.registerHandler);
 
-router.get('/plants', plantsController);
+router.get('/plants', plantsController.plantsHandler);
+router.post('/plants', plantsController.postPlantsHandler);
 
-router.get('/plants/:email', plantsByUserController);
-router.post('/plants/:email', plantsByUserController);
+router.get('/plantsbyuser', plantsByUserController.getPlantsByUserHandler);
+router.post('/plantsbyuser', plantsByUserController.postPlantsByUserHandler);
 
-router.post('/user', userController);
+router.post('/user', userController.userHandler);
 
 module.exports = router;
