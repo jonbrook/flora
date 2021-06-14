fbfed0e9f5f007e78a6fe091389bbcc7468ebb81
davidspanjaard <dmspanjaard@gmail.com>
2021-06-14 15:50:39 +0200

    Plant controllers working

--- server/controllers/plantController.js
+++ server/controllers/plantController.js
@@ -1,15 +1,35 @@
-// const { Plants } = require('../models/plantModel.js');
+const db = require('../models/postgres.js');
 
 const plantsHandler = async (req, res) => {
   try {
-    // const plants = await Plants.findAll();
-
-    res.status(200).send(' hi ');
+    const plants = await db.Plant.findAll();
+    res.status(200).send(plants);
   } catch (error) {
     res.status(400);
   }
 };
 
+const postPlantsHandler = async (req, res) => {
+  console.log(req.body);
+  try {
+    console.log(req.body.scientificName);
+    const plant = await db.Plant.create({
+      scientificName: req.body.scientificName,
+      commonName: req.body.commonName,
+      sunlightAmount: req.body.sunlightAmount,
+      waterAmount: req.body.waterAmount,
+      waterFrequency: req.body.waterFrequency,
+      airPurifying: req.body.airPurifying,
+      humidity: req.body.humidity,
+      soilMoisture: req.body.soilMoisture,
+    });
+    console.log(plant);
+    res.status(200).send(plant);
+  } catch (error) {
+    res.status(400);
+  }
+};
 module.exports = {
   plantsHandler,
+  postPlantsHandler,
 };
