78079e516bdd6bf335c20aa2d46c32f2e8dccd7b
Jon Brook <jonbrook89@gmail.com>
2021-06-17 10:07:44 +0100

    fix: rollback accidental changes in client linting

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,25 +1,4 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
-
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    'no-empty-function': 'off',
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+  root: true,
+  extends: '@react-native-community',
 };
