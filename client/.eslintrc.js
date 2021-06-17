ef06b5bc4333fd21b6e0282f43564feff159bcd8
Jon Brook <jonbrook89@gmail.com>
2021-06-17 09:36:06 +0100

    feat: add travis config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
