0695a65c27126d7c58aaddd6a5767afd59128f00
Jon Brook <jonbrook89@gmail.com>
2021-06-20 17:13:53 +0100

    Add redux provider to the app

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
+import { NativeRouter, Switch, Route } from 'react-router-native';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import LandingScreen from './screens/LandingScreen';
@@ -7,29 +10,26 @@ import CameraScreen from './screens/CameraScreen';
 import PlantListScreen from './screens/PlantListScreen';
 import PlantDetailsScreen from './screens/PlantDetailsScreen';
 require('./behaviorSubjects');
-// import autoMLKey from './apiKeys/key.json';
-
-// process.env.GOOGLE_APPLICATION_CREDENTIALS = autoMLKey;
-
-import { NativeRouter, Switch, Route } from 'react-router-native';
 
 const App = () => {
   return (
     <NativeRouter>
-      <View style={{ flex: 1 }}>
-        <Switch>
-          <Route exact path="/" component={LandingScreen} />
-          <Route exact path="/LoginScreen" component={LoginScreen} />
-          <Route exact path="/RegisterScreen" component={RegisterScreen} />
-          <Route exact path="/CameraScreen" component={CameraScreen} />
-          <Route exact path="/PlantListScreen" component={PlantListScreen} />
-          <Route
-            exact
-            path="/PlantDetailsScreen"
-            component={PlantDetailsScreen}
-          />
-        </Switch>
-      </View>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <Switch>
+            <Route exact path="/" component={LandingScreen} />
+            <Route exact path="/LoginScreen" component={LoginScreen} />
+            <Route exact path="/RegisterScreen" component={RegisterScreen} />
+            <Route exact path="/CameraScreen" component={CameraScreen} />
+            <Route exact path="/PlantListScreen" component={PlantListScreen} />
+            <Route
+              exact
+              path="/PlantDetailsScreen"
+              component={PlantDetailsScreen}
+            />
+          </Switch>
+        </View>
+      </Provider>
     </NativeRouter>
   );
 };
