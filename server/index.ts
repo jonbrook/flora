4c304e8611e956ea94473671744e16c034111e86
KonGkal <konsgkal@gmail.com>
2021-06-23 19:31:24 +0100

    fix: all, disable rules in loginController.test

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ const PORT = 3001;
 
 (async () => {
   await sequelize.sync();
+  // eslint-disable-next-line no-console
   console.log('database connection successful 👻');
   app.listen(PORT, () => {
     // eslint-disable-next-line no-console
