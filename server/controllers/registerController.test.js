b480ca0246c0637c32f1a7206e171f8a8214e702
KonGkal <konsgkal@gmail.com>
2021-06-23 19:43:41 +0100

    add: sequelize import in registerController test

--- server/controllers/registerController.test.js
+++ server/controllers/registerController.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import app from '../server';
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import sequelize from '../models/postgres';
 
 const users = {
   new: {
